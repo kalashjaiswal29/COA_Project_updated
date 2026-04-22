d6e19200b78be7ba3fab04ffec7244a0885a12c2
kalashjaiswal29 <kalashjaiswal57@gmail.com>
2026-04-22 11:03:44 +0530

    Fix: Multistudent face recognition and pymongo integration

--- python-cv/main.py
+++ python-cv/main.py
@@ -7,9 +7,7 @@ import urllib.request
 import gc
 
 from contextlib import asynccontextmanager
-from dotenv import load_dotenv
 
-load_dotenv()
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
